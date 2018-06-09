d01b8c06aa61cb89ad67b2d23f04c7da7951a321
noah398 <869tjmw@gmail.com>
2018-06-09 16:25:11 +0900

    style: add chat wrapper styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import styled from 'styled-components';
 import Form from '../Form/Form.js';
 import firebase from 'firebase';
 
@@ -29,34 +29,49 @@ class App extends Component {
 
     render () {
         return (
-            <div className="app">
-                <div className="app__header">
-                    {/*<img src={logo} className="app__logo" alt="logo" />*/}
-                    <h3>
-                        CHAT APP WITH REACT
-                    </h3>
-                    {!this.state.user ? (
-                        <button
-                            className="app__button"
-                            onClick={this.handleSignIn.bind(this)}
-                        >
-                            Sign in
-                        </button>
-                    ) : (
-                        <button
-                            className="app__button"
-                            onClick={this.handleLogOut.bind(this)}
-                        >
-                            Logout
-                        </button>
-                    )}
-                </div>
-                <div className="app__list">
-                    <Form user={this.state.user}/>
-                </div>
-            </div>
+            <Wrapper>
+                {!this.state.user ? (
+                    <button
+                        className="app__button"
+                        onClick={this.handleSignIn.bind(this)}
+                    >
+                        Sign in
+                    </button>
+                ) : (
+                    <button
+                        className="app__button"
+                        onClick={this.handleLogOut.bind(this)}
+                    >
+                        Logout
+                    </button>
+                )}
+
+                <Form user={this.state.user}/>
+            </Wrapper>
         );
     }
 }
+
+const Wrapper = styled.div`
+    @media(min-width: 426px){
+        height: 678px;
+        width: 314px;
+        border: 12px solid #080808;
+        border-radius: 44px;
+        box-shadow: 0 19px 38px rgba(0, 0, 0, 0.30), 0 15px 12px rgba(0, 0, 0, 0.22);
+        position: relative;
+        margin: 32px auto;
+        &:before {
+            position: absolute;
+            left: calc(50% - 28%);
+            top: 0;
+            width: 56%;
+            background: #080808;
+            height: 26px;
+            content: "";
+            border-radius: 0 0 15px 15px;
+        }
+    }
+`;
 
 export default App;
