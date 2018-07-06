797e8d344f2e921dbb73da02d08bf25b16426b70
noah398 <869tjmw@gmail.com>
2018-07-06 23:56:46 +0900

    style: add setting page style

--- src/components/pages/SettingPage/SettingPage.js
+++ src/components/pages/SettingPage/SettingPage.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import firebase from 'firebase'
 import ApplePhoneTemplate from '../../templates/ApplePhoneTemplate'
+import styled from 'styled-components';
 
 class SettingPage extends Component {
     constructor () {
@@ -27,18 +28,63 @@ class SettingPage extends Component {
     render () {
         return (
             <ApplePhoneTemplate>
-                <h3>Setting Page</h3>
                 {this.state.user &&
-                <div>
-                    <p>{this.state.user.displayName}</p>
-                    <p>{this.state.user.email}</p>
-                    <p>{this.state.user.uid}</p>
-                </div>
+                <ProfileWrapper>
+                    <Profile>
+                        <Avatar src={this.state.user.photoURL}/>
+                        <Name>{this.state.user.displayName}</Name>
+                        <Email>{this.state.user.email}</Email>
+                        <LogoutButton onClick={this.handleLogOut}>Logout</LogoutButton>
+                    </Profile>
+                </ProfileWrapper>
                 }
-                <button onClick={this.handleLogOut}>Logout</button>
             </ApplePhoneTemplate>
         );
     }
 }
+
+const Avatar = styled.img`
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    box-shadow: rgba(0, 0, 0, 0.54) 0 0 60px;
+`;
+
+const ProfileWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    background: linear-gradient(to left bottom, ${props => props.theme.greyLight}, ${props => props.theme.grey});
+`;
+
+const Profile = styled.div`
+    text-align: center;
+`;
+
+const Name = styled.span`
+    display: block;
+    color: ${props => props.theme.dark};
+    font-size: 1.3rem;
+    font-weight: 600;
+    margin-top: 16px;
+`;
+
+const Email = styled.span`
+    display: block;
+    color: ${props => props.theme.greyDark};
+    font-size: 0.8rem;
+    margin-bottom: 8px;
+`;
+
+const LogoutButton = styled.button`
+        background-color: transparent;
+        border: ${props => props.theme.dark} solid 1px;
+        color: ${props => props.theme.dark};
+        cursor: pointer;
+        outline: none;
+        padding: 2px 8px;
+        appearance: none;
+`;
 
 export default SettingPage;
